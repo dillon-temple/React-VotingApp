cd0fb5c78d8e48cbf542da4c3d5a022806c25823
Dillon Temple <momo@Dillons-MacBook-Pro.local>
2019-12-12 05:27:12 -0800

    counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import './App.css';
 
+class App extends React.Component{
+  constructor(props){
+    super(props);
 
+    this.state = {
+      counter: 0
+    }
+  }
 
-const App = (props) => {
-  return (
-    <div className="container">
-      <div className="counter">34</div>
-      <div className="framework">{props.framework}</div>
-      <button className="button" onClick={() => alert("You clicked " + props.framework + "!")}>Click Me!</button>
-    </div>
-  );
+  render(){
+      return (
+        <div className="container">
+          <div className="counter">{this.state.counter}</div>
+          <div className="framework">{this.props.framework}</div>
+          <button
+            className="button"
+            onClick={() => this.setState((previous) => ({
+              excitement: previous.counter++
+            }))}
+          >
+            Click Me!
+          </button>
+        </div>
+      );
+  }
+
 }
+
+// const App = (props) => {
+//   return (
+//     <div className="container">
+//       <div className="counter">34</div>
+//       <div className="framework">{props.framework}</div>
+//       <button className="button" onClick={() => alert("You clicked " + props.framework + "!")}>Click Me!</button>
+//     </div>
+//   );
+// }
 
 export default App;
